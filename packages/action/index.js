b2f00381ff63e32e1e032e7a8aacb25671749f98
Jacob Bolda <me@jacobbolda.com>
2020-06-01 16:02:32 -0500

    add action

---
+++ packages/action/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { covector } = require("../covector");
+
+try {
+  const command = core.getInput("command");
+  const c = covector({ command });
+  core.setOutput("change", c);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
