b2894ffaf0eaffe56f58d214cd19582ea80d1197
Jacob Bolda <me@jacobbolda.com>
2020-06-02 09:01:35 -0500

    try to full action with async

--- packages/action/index.js
+++ packages/action/index.js
@@ -1,27 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-// const { covector } = require("../covector");
+const { covector } = require("../covector");
 
 async function run() {
   try {
-    console.log("place 1");
     const inputCommand = core.getInput("command");
-    console.log("place 2");
     let command = inputCommand;
-    console.log("place 3");
-    // if (inputCommand === "version-or-publish") {
-    //   if (yield covector({ command: "status" }) === "No changes.") {
-    //     command = "publish";
-    //   } else {
-    //     command = "version";
-    //   }
-    // }
-    console.log("place 4");
-    // const covectored = yield covector({ command });
-    const covectored = { stuff: "things" };
-    console.log("place 5");
+    if (inputCommand === "version-or-publish") {
+      if ((await covector({ command: "status" })) === "No changes.") {
+        command = "publish";
+      } else {
+        command = "version";
+      }
+    }
+    const covectored = await covector({ command });
     core.setOutput("change", covectored);
-    console.log("place 6");
     const payload = JSON.stringify(covectored, undefined, 2);
     console.log(`The covector output: ${payload}`);
   } catch (error) {
