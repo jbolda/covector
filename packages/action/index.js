40041a5dbcd7caf061dc0b7cf89a88483b868280
Jacob Bolda <me@jacobbolda.com>
2020-06-02 08:31:27 -0500

    try skipping covector completely

--- packages/action/index.js
+++ packages/action/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { covector } = require("../covector");
+// const { covector } = require("../covector");
 
 function* run() {
   try {
@@ -9,15 +9,16 @@ function* run() {
     console.log("place 2");
     let command = inputCommand;
     console.log("place 3");
-    if (inputCommand === "version-or-publish") {
-      if (yield covector({ command: "status" }) === "No changes.") {
-        command = "publish";
-      } else {
-        command = "version";
-      }
-    }
+    // if (inputCommand === "version-or-publish") {
+    //   if (yield covector({ command: "status" }) === "No changes.") {
+    //     command = "publish";
+    //   } else {
+    //     command = "version";
+    //   }
+    // }
     console.log("place 4");
-    const covectored = yield covector({ command });
+    // const covectored = yield covector({ command });
+    const covectored = { stuff: "things" };
     console.log("place 5");
     core.setOutput("change", covectored);
     console.log("place 6");
@@ -28,7 +29,6 @@ function* run() {
   }
 }
 
-console.log("does it reach here");
 run();
 
 export default run;
