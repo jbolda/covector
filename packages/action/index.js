08c6b7b591626f4a5637a2c004da4a16f9f33307
Jacob Bolda <me@jacobbolda.com>
2020-06-02 07:52:27 -0500

    wrap action in function

--- packages/action/index.js
+++ packages/action/index.js
@@ -2,21 +2,27 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { covector } = require("../covector");
 
-try {
-  const command = core.getInput("command");
-  console.log = core.debug;
-  let commandToRun = command;
-  if (command === "version-or-publish") {
-    if (covector({ command: "status" }) === "No changes.") {
-      commandToRun = "publish";
-    } else {
-      commandToRun = "version";
+function* run() {
+  try {
+    const command = core.getInput("command");
+    console.log = core.debug;
+    let commandToRun = command;
+    if (command === "version-or-publish") {
+      if (yield covector({ command: "status" }) === "No changes.") {
+        commandToRun = "publish";
+      } else {
+        commandToRun = "version";
+      }
     }
+    const covectored = yield covector({ command: commandToRun });
+    core.setOutput("change", covectored);
+    const payload = JSON.stringify(covectored, undefined, 2);
+    console.log(`The covector output: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  const covectored = covector({ command: commandToRun });
-  core.setOutput("change", covectored);
-  const payload = JSON.stringify(covectored, undefined, 2);
-  console.log(`The covector output: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
+
+export default run;
