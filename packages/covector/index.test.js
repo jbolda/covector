c431cc58a9ac2f9b57a1893638baf3370385f0b4
Jacob Bolda <me@jacobbolda.com>
2020-06-09 07:32:58 -0500

    add test for covector status

---
+++ packages/covector/index.test.js
@@ -0,0 +1,21 @@
+const { covector } = require("./index");
+const mockConsole = require("jest-mock-console");
+const fixtures = require("fixturez");
+const f = fixtures(__dirname);
+
+describe("integration test", () => {
+  it("passes correct config for js and rust", function* () {
+    const restoreConsole = mockConsole(["log", "dir"]);
+    const fullIntegration = f.copy("integration.js-and-rust-with-changes");
+    const covectored = yield covector({
+      command: "status",
+      cwd: fullIntegration,
+    });
+    expect({
+      consoleLog: console.log.mock.calls,
+      consoleDir: console.dir.mock.calls,
+      covectorReturn: covectored,
+    }).toMatchSnapshot();
+    restoreConsole();
+  });
+});
