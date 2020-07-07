4f715b378d7a7e7da5385740e0d6c08b0c4f2772
Jacob Bolda <me@jacobbolda.com>
2020-07-07 08:33:57 -0500

    rough in release creation

--- packages/action/index.js
+++ packages/action/index.js
@@ -1,10 +1,11 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+const github, {context} = require("@actions/github");
 const { main } = require("@effection/node");
 const { covector } = require("../covector");
 
 main(function* run() {
   try {
+    const token = core.getInput("token") || process.env.GITHUB_TOKEN;
     const inputCommand = core.getInput("command");
     let command = inputCommand;
     if (inputCommand === "version-or-publish") {
@@ -19,6 +20,18 @@ main(function* run() {
     core.setOutput("change", covectored);
     const payload = JSON.stringify(covectored, undefined, 2);
     console.log(`The covector output: ${payload}`);
+
+    const octokit = github.getOctokit(token);
+    const { owner, repo } = context.repo;
+    Object.keys(covectored).forEach(pkg => {
+      yield octokit.repos.createRelease({
+        owner,
+        repo,
+        tag_name: pkg,
+        name: pkg,
+        body: `${covectored[pkg].precommand}\n${covectored[pkg].command}\n${covectored[pkg].postcommand}\n`,
+      });
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
