a1ac9f7b03be0a76bf3cfb664f330fc29e5c0c4e
Jacob Bolda <me@jacobbolda.com>
2020-07-08 00:19:42 -0500

    feature: github release on publish (#76)
    * rough in release creation
    
    * build before running status on PR
    
    * return pkg in publish return
    
    * remove tag from publish
    
    * update action to output version smushed for PR and publish to create releases
    
    * try publish in cavalier mode
    
    * run yarn before hand
    
    * update @actions/github
    
    * fix name
    
    * set token
    
    * opt into releases
    
    * token needs to be specified
    
    * test version output
    
    * build before attempt
    
    * update tests with new return
    
    * update action version text
    
    * button up workflows for finale

--- packages/action/index.js
+++ packages/action/index.js
@@ -5,6 +5,10 @@ const { covector } = require("../covector");
 
 main(function* run() {
   try {
+    const token =
+      core.getInput("token") === ""
+        ? process.env.GITHUB_TOKEN
+        : core.getInput("token");
     const inputCommand = core.getInput("command");
     let command = inputCommand;
     if (inputCommand === "version-or-publish") {
@@ -16,10 +20,53 @@ main(function* run() {
       }
     }
     const covectored = yield covector({ command });
-    core.setOutput("change", covectored);
-    const payload = JSON.stringify(covectored, undefined, 2);
-    console.log(`The covector output: ${payload}`);
+
+    if (command === "version") {
+      const covectoredSmushed = Object.keys(covectored).reduce((text, pkg) => {
+        if (typeof covectored[pkg].command === "string") {
+          text = `${text}\n\n\n# ${pkg}\n\n${commandText(covectored[pkg])}`;
+        }
+        return text;
+      }, "# Version Updates\n\nMerging this PR will bump all of the applicable packages based on your change files.\n\n");
+      core.setOutput("change", covectoredSmushed);
+      const payload = JSON.stringify(covectoredSmushed, undefined, 2);
+      console.log(`The covector output: ${payload}`);
+    } else if (command === "publish" && core.getInput("createRelease")) {
+      core.setOutput("change", covectored);
+      const payload = JSON.stringify(covectored, undefined, 2);
+      console.log(`The covector output: ${payload}`);
+      const octokit = github.getOctokit(token);
+      const { owner, repo } = github.context.repo;
+      let releases = {};
+      for (let pkg of Object.keys(covectored)) {
+        if (covectored[pkg].command !== false || true) {
+          // true to test
+          const createReleaseResponse = yield octokit.repos.createRelease({
+            owner,
+            repo,
+            tag_name: `${pkg}-v${covectored[pkg].pkg.pkgFile.version}`,
+            name: `${pkg} v${covectored[pkg].pkg.pkgFile.version}`,
+            body: commandText(covectored[pkg]),
+          });
+          const { data } = createReleaseResponse;
+          releases[pkg] = data; // { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl }
+        }
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 });
+
+const commandText = (pkg) => {
+  const { precommand, command, postcommand } = pkg;
+  let text = "";
+  if (typeof precommand !== "boolean") {
+    text = `${precommand}\n`;
+  } else if (typeof command !== "boolean") {
+    text = `${command}\n`;
+  } else if (typeof postcommand !== "boolean") {
+    text = `${postcommand}\n`;
+  }
+  return text === "" ? "Publish complete." : text;
+};
