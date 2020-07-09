9d72ebcea24fdc08f7bdf652ee3558c3c56fe029
Jacob Bolda <me@jacobbolda.com>
2020-07-08 23:06:29 -0500

    pull out commands into separate package

--- packages/covector/src/run.js
+++ packages/covector/src/run.js
@@ -1,11 +1,8 @@
-const { spawn, timeout } = require("effection");
-const execa = require("execa");
-const { once, on } = require("@effection/events");
+const { attemptCommands, raceTime } = require("@covector/command");
 const { configFile, changeFiles } = require("@covector/files");
 const { assemble, mergeIntoConfig } = require("@covector/assemble");
 const { fillChangelogs } = require("@covector/changelog");
 const { apply, changesConsideringParents } = require("@covector/apply");
-const path = require("path");
 
 module.exports.covector = function* covector({
   command,
@@ -150,127 +147,5 @@ module.exports.covector = function* covector({
     });
 
     return pkgCommandsRan;
-  }
-};
-
-function raceTime(
-  t = 120000,
-  msg = `timeout out waiting ${t / 1000}s for command`
-) {
-  return spawn(function* () {
-    yield timeout(t);
-    throw new Error(msg);
-  });
-}
-
-const attemptCommands = function* ({
-  cwd,
-  commands,
-  command,
-  commandPrefix = "",
-  pkgCommandsRan,
-  dryRun,
-}) {
-  let _pkgCommandsRan = { ...pkgCommandsRan };
-  for (let pkg of commands) {
-    if (!!pkg.getPublishedVersion) {
-      const version = yield runCommand({
-        command: pkg.getPublishedVersion,
-        cwd,
-        pkg: pkg.pkg,
-        pkgPath: pkg.path,
-        stdio: "pipe",
-        log: `Checking if ${pkg.pkg}@${pkg.pkgFile.version} is already published with: ${pkg.getPublishedVersion}`,
-      });
-
-      if (pkg.pkgFile.version === version) {
-        console.log(
-          `${pkg.pkg}@${pkg.pkgFile.version} is already published. Skipping.`
-        );
-        continue;
-      }
-    }
-    if (!pkg[`${commandPrefix}command`]) continue;
-    const pubCommands =
-      typeof pkg[`${commandPrefix}command`] === "string" ||
-      !Array.isArray(pkg[`${commandPrefix}command`])
-        ? [pkg[`${commandPrefix}command`]]
-        : pkg[`${commandPrefix}command`];
-    let stdout = "";
-    for (let pubCommand of pubCommands) {
-      const runningCommand = {};
-      if (
-        typeof pubCommand === "object" &&
-        pubCommand.dryRunCommand === false
-      ) {
-        runningCommand.command = pubCommand.command;
-        runningCommand.shouldRunCommand = !dryRun;
-      } else if (typeof pubCommand === "object") {
-        // dryRunCommand will either be a !string (false) or !undefined (true) or !true (false)
-        if (pubCommand.dryRunCommand === true) {
-          runningCommand.command = pubCommand.command;
-          runningCommand.shouldRunCommand = true;
-        } else {
-          runningCommand.command = !pubCommand.dryRunCommand
-            ? pubCommand.command
-            : pubCommand.dryRunCommand;
-          runningCommand.shouldRunCommand = !dryRun;
-        }
-      } else {
-        runningCommand.command = pubCommand;
-        runningCommand.shouldRunCommand = !dryRun;
-      }
-
-      if (runningCommand.shouldRunCommand) {
-        const ranCommand = yield runCommand({
-          command: runningCommand.command,
-          cwd,
-          pkg: pkg.pkg,
-          pkgPath: pkg.path,
-          log: `${pkg.pkg} [${commandPrefix}${command}]: ${runningCommand.command}`,
-        });
-
-        if (pubCommand.pipe) {
-          stdout = `${stdout}${ranCommand}\n`;
-        }
-      } else {
-        console.log(
-          `dryRun >> ${pkg.pkg} [${commandPrefix}${command}]: ${runningCommand.command}`
-        );
-      }
-    }
-
-    if (!!pkgCommandsRan)
-      _pkgCommandsRan[pkg.pkg][`${commandPrefix}command`] =
-        stdout !== "" ? stdout : true;
-  }
-  return _pkgCommandsRan;
-};
-
-const runCommand = function* ({
-  pkg,
-  command,
-  cwd,
-  pkgPath,
-  stdio = "pipe",
-  log = `running command for ${pkg}`,
-}) {
-  let child;
-  try {
-    return yield function* () {
-      console.log(log);
-      child = yield execa.command(command, {
-        cwd: path.join(cwd, pkgPath),
-        shell: process.env.shell || true,
-        windowsHide: true,
-        all: true,
-        env: { FORCE_COLOR: 0 },
-      });
-
-      console.log(child.all);
-      return child.all;
-    };
-  } catch (error) {
-    throw error;
   }
 };
