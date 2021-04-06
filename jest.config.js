cf8b3a22f06a21b59a688defe7efb55c7f159870
Jacob Bolda <me@jacobbolda.com>
2021-04-06 16:51:04 -0500

    update test snaps for npm switch

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["__fixtures__", "dist"],
+  modulePathIgnorePatterns: ["__fixtures__"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.base.json",
+    },
+  },
 };
