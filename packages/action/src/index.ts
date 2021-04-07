f8c0bb9c32233537e97178df8969741dfb879ec2
minkimcello <minkimcello@gmail.com>
2021-04-07 15:57:33 -0400

    Add conditional for preview command

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -104,6 +104,47 @@ export function* run(): Generator<any, any, any> {
         console.log(`The covector output: ${payload}`);
         return covectored;
       }
+    } else if (command === "preview") {
+      const previewLabel = github.context.payload.pull_request.labels.filter(({ name }) => name === 'preview').length;
+      if (!previewLabel) {
+        console.log('Not publishing any preview packages because the "preview" label has not been applied to this pull request.')
+      } else {
+        let covectored: Covector;
+        // if (core.getInput("createRelease") === "true" && token) {
+        //   const octokit = github.getOctokit(token);
+        //   const { owner, repo } = github.context.repo;
+        //   covectored = yield covector({
+        //     command,
+        //     filterPackages,
+        //     cwd,
+        //     modifyConfig: injectPublishFunctions([
+        //       createReleases({ core, octokit, owner, repo }),
+        //     ]),
+        //   });
+        // } else {
+          covectored = yield covector({
+            command,
+            filterPackages,
+            cwd,
+          });
+        // }
+
+        // if (covectored) {
+        //   let packagesPublished = Object.keys(covectored).reduce((pub, pkg) => {
+        //     if (!covectored[pkg].published) {
+        //       return pub;
+        //     } else {
+        //       return `${pub}${pkg}`;
+        //     }
+        //   }, "");
+        //   core.setOutput("packagesPublished", packagesPublished);
+          // for (let pkg of Object.keys(covectored)) {
+          //   if (covectored[pkg].command !== false) successfulPublish = true;
+          // }
+          // core.setOutput("successfulPublish", successfulPublish);
+          // core.setOutput("change", covectored);
+        // }
+      }
     } else {
       throw new Error(`Command "${command}" not recognized. See README for which commands are available.`);
     };
