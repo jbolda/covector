4236aab437bdaa2d7c38d85f8898fb2b4f9f5269
minkimcello <minkimcello@gmail.com>
2021-04-14 10:59:24 -0400

    Pass in context and previewVersion into covector from action

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -105,45 +105,38 @@ export function* run(): Generator<any, any, any> {
         return covectored;
       }
     } else if (command === "preview") {
-      const previewLabel = github?.context?.payload?.pull_request?.labels?.filter(({ name } : { name: String }) => name === 'preview').length;
+      const configuredLabel = core.getInput("label");
+      const previewLabel = github?.context?.payload?.pull_request?.labels?.filter(({ name } : { name: String }) => name === configuredLabel).length;
+      const previewVersion = core.getInput("previewVersion");
+
       if (!previewLabel) {
-        console.log('Not publishing any preview packages because the "preview" label has not been applied to this pull request.')
+        console.log(`Not publishing any preview packages because the "${configuredLabel}" label has not been applied to this pull request.`);
       } else {
         let covectored: Covector;
-        // if (core.getInput("createRelease") === "true" && token) {
-        //   const octokit = github.getOctokit(token);
-        //   const { owner, repo } = github.context.repo;
-        //   covectored = yield covector({
-        //     command,
-        //     filterPackages,
-        //     cwd,
-        //     modifyConfig: injectPublishFunctions([
-        //       createReleases({ core, octokit, owner, repo }),
-        //     ]),
-        //   });
-        // } else {
-          covectored = yield covector({
-            command,
-            filterPackages,
-            cwd,
-          });
-        // }
 
-        // if (covectored) {
-        //   let packagesPublished = Object.keys(covectored).reduce((pub, pkg) => {
-        //     if (!covectored[pkg].published) {
-        //       return pub;
-        //     } else {
-        //       return `${pub}${pkg}`;
-        //     }
-        //   }, "");
-        //   core.setOutput("packagesPublished", packagesPublished);
-          // for (let pkg of Object.keys(covectored)) {
-          //   if (covectored[pkg].command !== false) successfulPublish = true;
-          // }
-          // core.setOutput("successfulPublish", successfulPublish);
-          // core.setOutput("change", covectored);
-        // }
+        covectored = yield covector({
+          command,
+          filterPackages,
+          cwd,
+          context: github.context.payload,
+          previewVersion
+        });
+
+        if (covectored) {
+          let packagesPublished = Object.keys(covectored).reduce((pub, pkg) => {
+            if (!covectored[pkg].published) {
+              return pub;
+            } else {
+              return `${pub}${pkg}`;
+            }
+          }, "");
+          core.setOutput("packagesPublished", packagesPublished);
+          for (let pkg of Object.keys(covectored)) {
+            if (covectored[pkg].command !== false) successfulPublish = true;
+          }
+          core.setOutput("successfulPublish", successfulPublish);
+          core.setOutput("change", covectored);
+        }
       }
     } else {
       throw new Error(`Command "${command}" not recognized. See README for which commands are available.`);
