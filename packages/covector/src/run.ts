3d39a5bf7cf02b927a49d8906de9757112956b0b
minkimcello <minkimcello@gmail.com>
2021-04-16 14:43:31 -0400

    Undo doubling timeout for preview

--- packages/covector/src/run.ts
+++ packages/covector/src/run.ts
@@ -223,7 +223,7 @@ export function* covector({
 
     return pkgCommandsRan;
   } else if (command === "preview") {
-    yield raceTime({ t: config.timeout * 2 });
+    yield raceTime({ t: config.timeout });
 
     let versionTemplate;
     
