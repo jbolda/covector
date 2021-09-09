028ae8967a82c2dd8f8898f682599f5dcadb049d
Jonas Kruckenberg <iterpre@protonmail.com>
2021-09-09 19:48:21 +0200

    Release info in action output (#221)
    * Update utils.ts
    
    * debug log release id
    
    * Update utils.ts
    
    * include release info in action output
    
    * feat: include release information in action output
    
    * update prebuild version
    
    * update readme and action.yml
    
    * fix formatting
    
    * increase timeout for flaky test
    
    * Update index.test.ts.snap

--- packages/action/src/utils.ts
+++ packages/action/src/utils.ts
@@ -166,10 +166,13 @@ export const createReleases = curry(
       "true"
     );
 
-    // releaseResponse.upload_url is available on both responses
-    // considering putting that to the output
+    // output information about the created release
+    core.setOutput("releaseUrl", releaseResponse.url);
+    core.setOutput("releaseUploadUrl", releaseResponse.upload_url);
+    core.setOutput("releaseId", releaseResponse.id);
 
     core.startGroup(`github release created for ${pipe.pkg}`);
+    console.log("releaseId", releaseResponse.id);
     console.log("release created: ", releaseResponse);
     core.endGroup();
 
