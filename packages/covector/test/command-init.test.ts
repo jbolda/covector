423208379b15eeb426423fd0dc991b86fe1f198c
Jacob Bolda <me@jacobbolda.com>
2022-12-21 10:55:48 -0600

    crush init test flake (#255)
    * first test passing consisntetly.... maybe
    
    * seriously.... no line breaks then
    
    * update for workspace test
    
    * are carriage returns making a difference
    
    * ok now
    
    * use responded text for snapshot instead, more consistent
    
    * trim for that consistency spike
    
    * better errors on timeout
    
    * get much more specific on question regex for workspace check
    
    * trim every line
    
    * gitSiteUrl text snap responded

--- packages/covector/test/command-init.test.ts
+++ packages/covector/test/command-init.test.ts
@@ -7,41 +7,47 @@ const f = fixtures(__dirname);
 describe("integration test for init command", () => {
   it("runs on a workspace", function* () {
     const fullIntegration = f.copy("pkg.js-yarn-workspace");
-    const { stdout, stderr, status } = yield runCommand(
+    const gitSiteUrl = "https://example.com";
+    const { stderr, status, responded } = yield runCommand(
       command("init", fullIntegration),
       fullIntegration,
-      "just press enter for everything right now"
-      // [
-      //   ["What is the url to your github repo?", "https://www.jacobbolda.com"],
-      //   ["should we include github action workflows?", "Y"],
-      //   ["What is the name of your default branch?", "pressEnter"],
-      // ]
+      [
+        [/^\? What is the url to your github repo\?$/, gitSiteUrl],
+        [/^\? should we include github action workflows\? \(Y\/n\)$/, "Y"],
+        [
+          /^\? What is the name of your default branch\? \(main\)$/,
+          "pressEnter",
+        ],
+      ]
     );
 
     expect(stderr).toBe("");
-    expect(stdout).toMatchSnapshot();
+    expect(responded).toMatchSnapshot();
     expect(status.code).toBe(0);
 
     // let's do a check to confirm it sets the config file correctly
     const config = yield loadFile("./.changes/config.json", fullIntegration);
     expect(config.path).toEqual(".changes/config.json");
-    expect(JSON.parse(config.content).gitSiteUrl).toBe(undefined);
+    expect(JSON.parse(config.content).gitSiteUrl).toBe(`${gitSiteUrl}/`);
   }, 25000); // windows takes some time
 
   it("sets gitSiteUrl default to repo url", function* () {
     const fullIntegration = f.copy("pkg.js-single-json");
-    const { stdout, stderr, status } = yield runCommand(
+    const { responded, stderr, status } = yield runCommand(
       command("init", fullIntegration),
       fullIntegration,
       [
-        ["What is the url to your github repo?", "pressEnter"],
-        ["should we include github action workflows?", "Y"],
-        ["What is the name of your default branch?", "pressEnter"],
+        [/\? What is the url to your github repo\? \(.+\)$/, "pressEnter"],
+        [/\? should we include github action workflows\? \(Y\/n\)$/, "Y"],
+        [
+          /\? What is the name of your default branch\? \(main\)$/,
+          "pressEnter",
+        ],
       ]
     );
 
     expect(stderr).toBe("");
-    expect(stdout).toMatchSnapshot();
+    expect(responded).toMatchSnapshot();
     expect(status.code).toBe(0);
 
     // let's do a check to confirm it sets the config file correctly
