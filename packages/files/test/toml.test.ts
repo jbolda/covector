c3dcb6285f993afe00443021277c466b9b5b92c3
Jacob Bolda <me@jacobbolda.com>
2023-03-16 22:55:15 -0500

    support rust workspace inheritance

---
+++ packages/files/test/toml.test.ts
@@ -0,0 +1,145 @@
+import { readPkgFile, setPackageFileVersion, writePkgFile } from "../src";
+import { it } from "@effection/jest";
+import path from "path";
+import fixtures from "fixturez";
+import { PackageFile } from "@covector/types";
+const f = fixtures(__dirname);
+
+describe("toml", () => {
+  describe("parses", () => {
+    describe("single rust crate", () => {
+      const cargoFolder = f.copy("pkg.rust-single");
+
+      it("with file specified", function* () {
+        const cargoFile = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust-single-fixture",
+        });
+        expect(cargoFile.name).toBe("rust-single-fixture");
+        expect(cargoFile?.pkg?.package?.name).toBe("rust-single-fixture");
+        expect(cargoFile.version).toBe("0.5.0");
+      });
+
+      it("by deriving", function* () {
+        const cargoFile = yield readPkgFile({
+          cwd: cargoFolder,
+          pkgConfig: { manager: "rust", path: "." },
+          nickname: "rust-single-fixture",
+        });
+        expect(cargoFile.name).toBe("rust-single-fixture");
+        expect(cargoFile?.pkg?.package?.name).toBe("rust-single-fixture");
+        expect(cargoFile.version).toBe("0.5.0");
+      });
+    });
+
+    describe("rust workspace", () => {
+      it("without version inheritance", function* () {
+        // `pkg.rust-multi` is a virtual workspace
+        // https://doc.rust-lang.org/cargo/reference/workspaces.html#virtual-workspace
+        const cargoFolder = f.copy("pkg.rust-multi");
+
+        const cargoFilePkgA = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: path.join(cargoFolder, "pkg-a"),
+          nickname: "rust_pkg_a_fixture",
+        });
+        expect(cargoFilePkgA.name).toBe("rust_pkg_a_fixture");
+        expect(cargoFilePkgA?.pkg?.package?.name).toBe("rust_pkg_a_fixture");
+        expect(cargoFilePkgA.version).toBe("0.5.0");
+
+        const cargoFilePkgB = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: path.join(cargoFolder, "pkg-b"),
+          nickname: "rust_pkg_b_fixture",
+        });
+        expect(cargoFilePkgB.name).toBe("rust_pkg_b_fixture");
+        expect(cargoFilePkgB?.pkg?.package?.name).toBe("rust_pkg_b_fixture");
+        expect(cargoFilePkgB.version).toBe("0.8.8");
+      });
+
+      it("with version inheritance", function* () {
+        // `pkg.rust-multi` is a virtual workspace
+        // https://doc.rust-lang.org/cargo/reference/workspaces.html#virtual-workspace
+        const cargoFolder = f.copy("pkg.rust-multi-inheritance");
+
+        const cargoFilePkgA = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust_pkg_a_fixture",
+        });
+        expect(cargoFilePkgA?.pkg?.workspace?.package?.version).toBe("1.2.3");
+        expect(cargoFilePkgA.version).toBe("1.2.3");
+
+        const cargoFilePkgB = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: path.join(cargoFolder, "pkg-b"),
+          nickname: "rust_pkg_b_fixture",
+        });
+        expect(cargoFilePkgB.name).toBe("rust_pkg_b_fixture");
+        expect(cargoFilePkgB?.pkg?.package?.name).toBe("rust_pkg_b_fixture");
+        expect(cargoFilePkgB.version).toBe("0.8.8");
+      });
+    });
+  });
+
+  describe("writes", () => {
+    describe("rust workspace", () => {
+      it("without version inheritance", function* () {
+        // `pkg.rust-multi` is a virtual workspace
+        // https://doc.rust-lang.org/cargo/reference/workspaces.html#virtual-workspace
+        const cargoFolder = f.copy("pkg.rust-multi");
+
+        const cargoFilePkgA = yield readPkgFile({
+          file: "pkg-a/Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust_pkg_a_fixture",
+        });
+        const cargoFileMemoryPkgA = setPackageFileVersion({
+          pkg: cargoFilePkgA,
+          version: "4.5.6",
+          property: "version",
+        });
+        yield writePkgFile({
+          packageFile: cargoFileMemoryPkgA,
+          cwd: cargoFolder,
+        });
+
+        const cargoFileModifiedPkgA = yield readPkgFile({
+          file: "pkg-a/Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust_pkg_a_fixture",
+        });
+        expect(cargoFileModifiedPkgA.version).toBe("4.5.6");
+      });
+
+      it("with version inheritance", function* () {
+        // `pkg.rust-multi` is a virtual workspace
+        // https://doc.rust-lang.org/cargo/reference/workspaces.html#virtual-workspace
+        const cargoFolder = f.copy("pkg.rust-multi-inheritance");
+
+        const cargoFilePkgA = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust_pkg_a_fixture",
+        });
+        const cargoFileMemoryPkgA = setPackageFileVersion({
+          pkg: cargoFilePkgA,
+          version: "4.5.6",
+          property: "version",
+        });
+        yield writePkgFile({
+          packageFile: cargoFileMemoryPkgA,
+          cwd: cargoFolder,
+        });
+
+        const cargoFileModifiedPkgA = yield readPkgFile({
+          file: "Cargo.toml",
+          cwd: cargoFolder,
+          nickname: "rust_pkg_a_fixture",
+        });
+        expect(cargoFileModifiedPkgA.version).toBe("4.5.6");
+      });
+    });
+  });
+});
