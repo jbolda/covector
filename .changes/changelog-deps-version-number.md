00416af84014455a816749f9f1ae71a226eacd4f
Jacob Bolda <me@jacobbolda.com>
2023-06-19 23:13:06 -0500

    Add the version number to the changelog deps section

---
+++ .changes/changelog-deps-version-number.md
@@ -0,0 +1,6 @@
+---
+"@covector/apply": minor
+"@covector/changelog": minor
+---
+
+Add change for all exact deps rolled up to handle it with the changelog deps section. Add the version number to the changelog deps section.
--- packages/apply/src/parents.ts
+++ packages/apply/src/parents.ts
@@ -89,37 +89,33 @@ const parentBump = ({
             ?.version || "none";
         const versionRequirementMatch = /[\^=~]/.exec(prevDepVersion);
         // pkg is the parent and main is the child
-        if (
-          !changes[pkg] &&
-          (!versionRequirementMatch || prevDepVersion === "none")
-        ) {
+        if (!versionRequirementMatch || prevDepVersion === "none") {
           // if the parent doesn't have a release
           //   and it doesn't have the dependency as a range
           //   add one to adopt the next version of it's child
           //   but don't use the changes as we will refer to this dep
           //   bump otherwise and pulling up the changelog is not needed
-          changes[pkg] = {
-            changes: [],
-            parents: parents[pkg],
-            // prerelease will do bump the X in `-beta.X` if it is already a prerelease
-            // or it will do a prepatch if it isn't a prerelease
-            type: !prereleaseIdentifier ? "patch" : "prerelease",
-          };
-          // we also need to presume recursion to update the parents' parents
-          if (Object.values(parents[pkg]).length > 0) recurse = true;
-
-          changes[pkg].changes?.forEach((parentChange) => {
-            // this ends up overwriting in cases multiple bumps,
-            //   we should adjust this to accept multiple
-            if (
-              !parentChange.meta.dependencies ||
-              parentChange.meta.dependencies.length === 0
-            ) {
-              parentChange.meta.dependencies = [main];
-            } else {
-              parentChange.meta.dependencies.push(main);
-            }
-          });
+          if (!changes[pkg]) {
+            changes[pkg] = {
+              changes: [
+                { meta: { dependencies: [main] }, summary: "", releases: {} },
+              ],
+              parents: parents[pkg],
+              // prerelease will do bump the X in `-beta.X` if it is already a prerelease
+              // or it will do a prepatch if it isn't a prerelease
+              type: !prereleaseIdentifier ? "patch" : "prerelease",
+            };
+            // we also need to presume recursion to update the parents' parents
+            if (Object.values(parents[pkg]).length > 0) recurse = true;
+          } else {
+            // if we have have a release planned, add a skeleton so it shows
+            //  in the deps bump section
+            changes[pkg].changes?.push({
+              meta: { dependencies: [main] },
+              summary: "",
+              releases: {},
+            });
+          }
         }
       });
     }
