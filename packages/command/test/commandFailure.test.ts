6f3ded8a1e77d716be0284c1054490c272697ab3
Jacob Bolda <me@jacobbolda.com>
2023-06-29 15:54:26 -0500

    windows failed command differences

--- packages/command/test/commandFailure.test.ts
+++ packages/command/test/commandFailure.test.ts
@@ -43,8 +43,16 @@ describe("attemptCommand fails", () => {
       })
     );
 
-    //@ts-expect-error
-    expect(console.error.mock.calls[0][0].message).toBe("spawn boop ENOENT");
+    if (process.platform === "win32") {
+      const errorMessage =
+        "'boop' is not recognized as an internal or external command,\r\n" +
+        "operable program or batch file.";
+      //@ts-expect-error
+      expect(console.error.mock.calls[0][0]).toBe(errorMessage);
+    } else {
+      //@ts-expect-error
+      expect(console.error.mock.calls[0][0].message).toBe("spawn boop ENOENT");
+    }
   });
 
   it("retries a failed function", function* () {
@@ -63,13 +71,28 @@ describe("attemptCommand fails", () => {
       })
     );
 
-    //@ts-expect-error
-    expect(console.error.mock.calls[0][0].message).toBe("spawn boop ENOENT");
-    //@ts-expect-error
-    expect(console.error.mock.calls[1][0].message).toBe("spawn boop ENOENT");
-    //@ts-expect-error
-    expect(console.error.mock.calls[2][0].message).toBe("spawn boop ENOENT");
-    //@ts-expect-error
-    expect(console.error.mock.calls?.[3]?.[0]?.message).toBeUndefined();
+    if (process.platform === "win32") {
+      const errorMessage =
+        "'boop' is not recognized as an internal or external command,\r\n" +
+        "operable program or batch file.";
+      //@ts-expect-error
+      expect(console.error.mock.calls[0][0]).toBe(errorMessage);
+      //@ts-expect-error
+      expect(console.error.mock.calls[2][0]).toBe(errorMessage);
+      //@ts-expect-error
+      expect(console.error.mock.calls[4][0]).toBe(errorMessage);
+      // ts-expect-error
+      // expect(console.error.mock.calls[3]).toBeUndefined();
+    } else {
+      const errorMessage = "spawn boop ENOENT";
+      //@ts-expect-error
+      expect(console.error.mock.calls[0][0].message).toBe(errorMessage);
+      //@ts-expect-error
+      expect(console.error.mock.calls[1][0].message).toBe(errorMessage);
+      //@ts-expect-error
+      expect(console.error.mock.calls[2][0].message).toBe(errorMessage);
+      //@ts-expect-error
+      expect(console.error.mock.calls?.[3]?.[0]?.message).toBeUndefined();
+    }
   });
 });
