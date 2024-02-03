79f9452c66757815688c8b96802b174ca0c2755f
amrbashir <amr.bashir2015@gmail.com>
2024-02-04 01:31:47 +0200

    fix fetchCommand test

--- packages/command/test/fetchCommand.test.ts
+++ packages/command/test/fetchCommand.test.ts
@@ -173,11 +173,7 @@ describe("fetchCommand", () => {
       );
 
       expect(errored.message).toEqual(
-        `tauri request to https://crates.io/api/v1/crates/tauri/0.12.0 returned errors: [
-  {
-    "detail": "crate \`tauri\` does not have a version \`0.12.0\`"
-  }
-]`
+        "tauri request to https://crates.io/api/v1/crates/tauri/0.12.0 returned code 404: Not Found"
       );
     });
 
@@ -208,11 +204,7 @@ describe("fetchCommand", () => {
 
       expect(console.error as any).toBeCalledTimes(2);
       expect(errored.message).toEqual(
-        `tauri request to https://crates.io/api/v1/crates/tauri/0.12.0 returned errors: [
-  {
-    "detail": "crate \`tauri\` does not have a version \`0.12.0\`"
-  }
-]`
+        "tauri request to https://crates.io/api/v1/crates/tauri/0.12.0 returned code 404: Not Found"
       );
     });
   });
