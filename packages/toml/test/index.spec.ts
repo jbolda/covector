fd5bb19c6774cbb9ca6cec8c3a81c2c8564626db
amrbashir <amr.bashir2015@gmail.com>
2024-02-04 01:06:54 +0200

    test comment and formatting perservance

--- packages/toml/test/index.spec.ts
+++ packages/toml/test/index.spec.ts
@@ -6,13 +6,20 @@ version = "0.0.0"
 edition = "2021"
 publish = false
 
+# comment to be perserved
+
 [lib]
 crate-type = ["cdylib"]
 
 [dependencies]
-toml_edit = { version = "0.21", features = ["serde"] }
+toml_edit = { version = "0.21", features = [
+  "serde",
+  "alloc",
+  "std",
+] }
 wasm-bindgen = "0.2"
-js-sys = "0.3.66"`;
+js-sys = "0.3.66"
+`;
 
 describe("toml", () => {
   it("parses", function () {
@@ -30,5 +37,10 @@ describe("toml", () => {
     toml.set("package.version", "0.1.0");
     expect(toml.package.name).toBe("newName");
     expect(toml.get("package.version")).toBe("0.1.0");
+  });
+
+  it("perserves ordering and formatting", function () {
+    const toml = new TomlDocument(INPUT_TOML);
+    expect(toml.toString()).toBe(INPUT_TOML);
   });
 });
