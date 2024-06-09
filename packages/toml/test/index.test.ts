e8023faad8720642cef0f4b5e39f424043526fd1
Jacob Bolda <me@jacobbolda.com>
2024-06-08 19:18:01 -0500

    toml tests to vitest

--- packages/toml/test/index.test.ts
+++ packages/toml/test/index.test.ts
@@ -1,4 +1,6 @@
-const { TomlDocument } = require("../dist");
+import { describe, it } from "../../../helpers/test-scope.ts";
+import { expect } from "vitest";
+import { TomlDocument } from "../dist";
 
 const INPUT_TOML = `[package]
 name = "covector-toml"
@@ -22,38 +24,40 @@ js-sys = "0.3.66"
 `;
 
 describe("toml", () => {
-  it("parses", function () {
+  it("parses", function* () {
     const toml = new TomlDocument(INPUT_TOML);
-    expect(toml.package.name).toBe("covector-toml");
-    expect(toml.package.edition).toBe("2021");
-    expect(toml.lib["crate-type"][0]).toBe("cdylib");
-    expect(Array.isArray(toml.dependencies)).toBe(false);
-    expect(Array.isArray(toml.dependencies.toml_edit.features)).toBe(true);
-    expect(toml.package.description).toBe(undefined);
+    expect(toml.package.name).to.deep.equal("covector-toml");
+    expect(toml.package.edition).to.deep.equal("2021");
+    expect(toml.lib["crate-type"][0]).to.deep.equal("cdylib");
+    expect(Array.isArray(toml.dependencies)).to.deep.equal(false);
+    expect(Array.isArray(toml.dependencies.toml_edit.features)).to.deep.equal(
+      true
+    );
+    expect(toml.package.description).to.deep.equal(undefined);
   });
 
-  it("sets and gets", function () {
+  it("sets and gets", function* () {
     const toml = new TomlDocument(INPUT_TOML);
     toml.package.name = "newName";
     toml.set("package.version", "0.1.0");
     toml.dependencies["toml_edit"].version = "1.0.0";
-    expect(toml.package.name).toBe("newName");
-    expect(toml.get("package.version")).toBe("0.1.0");
-    expect(toml.dependencies["toml_edit"].version).toBe("1.0.0");
+    expect(toml.package.name).to.deep.equal("newName");
+    expect(toml.get("package.version")).to.deep.equal("0.1.0");
+    expect(toml.dependencies["toml_edit"].version).to.deep.equal("1.0.0");
   });
 
-  it("perserves ordering and formatting", function () {
+  it("perserves ordering and formatting", function* () {
     const toml = new TomlDocument(INPUT_TOML);
-    expect(toml.toString()).toBe(INPUT_TOML);
+    expect(toml.toString()).to.deep.equal(INPUT_TOML);
   });
 
-  it("in operator works fine", function () {
+  it("in operator works fine", function* () {
     const toml = new TomlDocument(INPUT_TOML);
-    expect("package" in toml).toBe(true);
-    expect("package.name" in toml).toBe(true);
-    expect("package.license" in toml).toBe(false);
-    expect("dependencies" in toml).toBe(true);
+    expect("package" in toml).to.deep.equal(true);
+    expect("package.name" in toml).to.deep.equal(true);
+    expect("package.license" in toml).to.deep.equal(false);
+    expect("dependencies" in toml).to.deep.equal(true);
     let dependencies = toml.dependencies;
-    expect("toml_edit" in dependencies).toBe(true);
+    expect("toml_edit" in dependencies).to.deep.equal(true);
   });
 });
