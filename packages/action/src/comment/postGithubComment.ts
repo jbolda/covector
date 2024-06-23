16e12270ba2a23d8aa9a34fb4365fd38643572d6
Jacob Bolda <me@jacobbolda.com>
2024-06-22 22:48:13 -0500

    upload as artifact when comment fails

--- packages/action/src/comment/postGithubComment.ts
+++ packages/action/src/comment/postGithubComment.ts
@@ -1,4 +1,7 @@
-import { GitHub } from "@actions/github/lib/utils";
+import { DefaultArtifactClient } from "@actions/artifact";
+import fs from "node:fs/promises";
+import path from "node:path";
+import type { GitHub } from "@actions/github/lib/utils";
 import { Operation } from "effection";
 import type { PullRequestPayload } from "./types";
 import { Logger } from "@covector/types";
@@ -34,21 +37,44 @@ export function* postGithubComment({
       comment.body.includes(tag)
     );
 
-  if (previousComment) {
-    logger.info("Updating comment in pull request.");
-    yield octokit.rest.issues.updateComment({
-      owner,
-      repo,
-      comment_id: previousComment.id,
-      body,
-    });
-  } else {
-    logger.info("Posting comment in pull request.");
-    yield octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number,
-      body,
-    });
+  // this can fail if the token doesn't have permissions
+  try {
+    if (previousComment) {
+      logger.info("Updating comment in pull request.");
+      yield octokit.rest.issues.updateComment({
+        owner,
+        repo,
+        comment_id: previousComment.id,
+        body,
+      });
+    } else {
+      logger.info("Posting comment in pull request.");
+      yield octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number,
+        body,
+      });
+    }
+  } catch (error) {
+    logger.error("Posting comment failed, creating artifact instead.");
+    const artifactRoot = process.env.RUNNER_TEMP ?? "..";
+    const artifactFilename = "covector-comment.md";
+    logger.debug(
+      `Writing comment body to ${path.join(artifactRoot, artifactFilename)}`
+    );
+    yield fs.writeFile(path.join(artifactRoot, artifactFilename), body);
+    const artifact = new DefaultArtifactClient();
+    logger.debug(
+      `Uploading comment from ${path.join(artifactRoot, artifactFilename)}`
+    );
+    yield artifact.uploadArtifact(
+      "covector-comment",
+      [artifactFilename],
+      artifactRoot,
+      {
+        retentionDays: 1,
+      }
+    );
   }
 }
