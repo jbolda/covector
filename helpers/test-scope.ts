433dceb150d05c4c87ab9c844d24ffaefc38efa2
Jacob Bolda <me@jacobbolda.com>
2025-02-10 00:08:28 -0600

    handle for test suite functions with context

--- helpers/test-scope.ts
+++ helpers/test-scope.ts
@@ -1,4 +1,4 @@
-import {  type Operation } from "effection";
+import { type Operation } from "effection";
 import { createTestAdapter } from "./test-adapter";
 
 import * as vitest from "vitest";
@@ -15,7 +15,7 @@ function describeWithScope(
 
     vitest.afterAll(async (suite: any) => {
       await suite.adapter.destroy();
-    })
+    });
 
     if (factory && typeof factory === "function") {
       factory(...args);
@@ -23,17 +23,37 @@ function describeWithScope(
   });
 }
 
-describeWithScope.only = vitest.describe.only;
+describeWithScope.only = function describeWithScope(
+  name: string | Function,
+  factory?: vitest.SuiteFactory
+): vitest.SuiteCollector {
+  return vitest.describe.only(name, (...args) => {
+    vitest.beforeAll((suite: any) => {
+      let parent = suite.suite?.adapter;
+      suite.adapter = createTestAdapter({ name: String(name), parent });
+    });
+
+    vitest.afterAll(async (suite: any) => {
+      await suite.adapter.destroy();
+    });
+
+    if (factory && typeof factory === "function") {
+      factory(...args);
+    }
+  });
+};
 describeWithScope.skip = vitest.describe.skip;
-describeWithScope.skipIf = vitest.describe.skipIf;
-describeWithScope.runIf = vitest.describe.runIf;
+describeWithScope.skipIf = (condition: any) =>
+  condition ? describeWithScope.skip : describeWithScope;
+describeWithScope.runIf = (condition: any) =>
+  condition ? describeWithScope : describeWithScope.skip;
 
 export const describe = <typeof vitest.describe>(<unknown>describeWithScope);
 
 export function beforeEach(op: () => Operation<void>): void {
   vitest.beforeEach(async (context: any) => {
     context.task.suite.adapter.addSetup(op);
-  })
+  });
 }
 
 export function it(
@@ -42,9 +62,13 @@ export function it(
   timeout?: number
 ): void {
   if (op) {
-    return vitest.it(desc, async (context: any) => {
-      context.task.suite.adapter.runTest(op);
-    }, timeout);
+    return vitest.it(
+      desc,
+      async (context: any) => {
+        context.task.suite.adapter.runTest(op);
+      },
+      timeout
+    );
   } else {
     return vitest.it.todo(desc);
   }
@@ -56,12 +80,24 @@ it.only = function only(
   timeout?: number
 ): void {
   if (op) {
-    return vitest.it.only(desc, async (context: any) => {
-      context.task.suite.adapter.runTest(op);
-    }, timeout);
+    return vitest.it.only(
+      desc,
+      async (context: any) => {
+        context.task.suite.adapter.runTest(op);
+      },
+      timeout
+    );
   } else {
     return vitest.it.skip(desc, () => {});
   }
+};
+
+it.skip = function skip(
+  desc: string,
+  op?: () => Operation<void>,
+  timeout?: number
+): void {
+  return vitest.it.skip(desc, () => {});
 };
 
 export function* captureError<T>(op: Operation<T>): Operation<Error> {
