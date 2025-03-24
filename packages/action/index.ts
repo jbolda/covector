c46a370d74ac2578eec6c98ff5900731f9eb6f7a
Jacob Bolda <me@jacobbolda.com>
2025-03-23 23:59:39 -0500

    function call in main

--- packages/action/index.ts
+++ packages/action/index.ts
@@ -1,10 +1,8 @@
-import { main } from "effection";
+import { main, type Operation } from "effection";
 import { run } from "./src/index.ts";
 import { pino } from "pino";
 import logStream from "./src/logger.ts";
 
 const stream = logStream();
 const logger = pino(stream);
-await main(function* () {
-  run(logger);
-});
+await main(() => run(logger) as Operation<void>);
